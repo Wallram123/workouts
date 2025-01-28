31b8dfb06232ff72a770ef8abdec2e50ae3a3ef1
Wallram123 <nilsson.walle@gmail.com>
2025-01-28 11:02:53 +0200

    aaaaaaaaaaa

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from endpoints.views import index  # Import the view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('endpoints.urls')),
+    path('', index),  # Root URL route
 ]
